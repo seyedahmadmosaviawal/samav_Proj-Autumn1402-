bedc53b48716c6c3624a22b736299907ac498f6f
Seyed-Ahmad-Mousavi-Awal <seyedahmadmosaviawal2@gmail.com>
2024-01-25 20:04:37 +0330

    Function For Checking The Existing of samav Added

--- samav.c
+++ samav.c
@@ -11,16 +11,67 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include <dirent.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #define global_thing "C:\\Users\\user\\Desktop\\c programs\\tamrin\\project_1402_autumn\\global"
+
+char* samav_exist(int argc, char* argv[]){
+
+    char tmp_cwd[1024];
+    char* tmp_cpy = (char*) malloc(1024 * sizeof(char));
+    getcwd(tmp_cwd, sizeof(tmp_cwd));
+    strcpy(tmp_cpy, tmp_cwd);
+    struct dirent *entry;
+    do {
+        // find samav:
+        DIR *dir = opendir(".");
+        if (dir == NULL) {
+            fprintf(stdout, "SAMAV : An Error Was Founded While Opening The Current Directory");
+            free(tmp_cpy);
+            return NULL;
+        }
+        while ((entry = readdir(dir)) != NULL) {
+            if (entry->d_type == DT_DIR && strcmp(entry->d_name, "samav") == 0){
+                return tmp_cpy;
+            }
+        }
+        closedir(dir);
+
+        // change cwd to parent
+        if (strcmp(tmp_cwd, "C:\\") != 0) {
+            if (chdir("..") != 0) {fprintf(stdout, "SAMAV : Unknown Error");free(tmp_cpy); return NULL;}
+        }
+
+        // update current working directory
+        if (getcwd(tmp_cwd, sizeof(tmp_cwd)) == NULL) {fprintf(stdout, "SAMAV : Unknown Error"); free(tmp_cpy); return NULL;}
+        strcpy(tmp_cpy, tmp_cwd);
+
+    } while (strcmp(tmp_cwd, "C:\\") != 0);
+    free(tmp_cpy);
+    return NULL;
+}
 
 int main(int argc, char* argv[]){
     // Less Than 2 Inputs:
     if (argc < 2){
-        fprintf(stdout, "SAMAV : Please Enter A Valid Command!\n");
+        fprintf(stdout, "SAMAV : Please Enter A Valid Command!");
         return 1;
     }
+
+    char Current[1024];
+    if(getcwd(Current, sizeof(Current)) == NULL) {fprintf(stdout, "SAMAV : There Is An Unknown Error Please Try Again!"); return 1;}
+    char* Samav_Root;
+    Samav_Root = samav_exist(argc, argv);
     // Menu:
+
+    if(strcmp(argv[1], "init") == 0){
+        
+    }
+
 
 
 
