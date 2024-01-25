92590e8722f8a7f085d0305ed856b7a0d63cbc65
Seyed-Ahmad-Mousavi-Awal <seyedahmadmosaviawal2@gmail.com>
2024-01-25 17:16:34 +0330

    Headers & Structures Added

---
+++ samav.c
@@ -0,0 +1,31 @@
+// In The Name Of God
+// samav Project
+// Autumn_Winter 1402
+
+// Seyed_Ahmad_Mousavi_Awal
+// 402106648
+
+// Address Of Global Thing:
+// C:\Users\user\Desktop\c programs\tamrin\project_1402_autumn\global
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+#define global_thing "C:\\Users\\user\\Desktop\\c programs\\tamrin\\project_1402_autumn\\global"
+
+int main(int argc, char* argv[]){
+    // Less Than 2 Inputs:
+    if (argc < 2){
+        fprintf(stdout, "SAMAV : Please Enter A Valid Command!\n");
+        return 1;
+    }
+    // Menu:
+
+
+
+
+
+
+    return 0;
+}
